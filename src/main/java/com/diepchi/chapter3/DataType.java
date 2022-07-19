408c55a416b34be68e9739e19985b67b0172fc77
chidiep <chidiep23122003@gmail.com>
2022-07-19 22:22:08 +0700

    day 1

---
+++ src/main/java/com/diepchi/chapter3/DataType.java
@@ -0,0 +1,22 @@
+/*
+ * @Date 2022-07-19
+ * @Title Data type
+ * - Primitive data type, MAXIMUM, MINIMUM
+ * - User define data type
+ * - ASCII, Unicode
+ * - Wrapper class
+ * - Char
+ */
+
+
+package com.diepchi.chapter3;
+
+public class DataType {
+    public static void main(String[] args) {
+
+        int a = 5; // primitive
+        Integer b = 6; // Use wrapper object (class)
+
+        System.out.println(a + b);
+    }
+}
